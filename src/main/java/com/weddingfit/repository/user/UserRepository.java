814547ee33e69c0dedb077bde5b269e812c3b58b
Hyewon Lee <hooho931@gmail.com>
2025-09-05 00:11:15 +0900

    [feat] User Repository 생성 #1

---
+++ src/main/java/com/weddingfit/repository/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.weddingfit.repository.user;
+
+import com.weddingfit.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByLoginId(String loginId); // 로그인시 User 조회용
+    boolean existsByLoginId(String loginId); // 회원가입시 중복 검사용
+    boolean existsByPhoneNumber(String phoneNumber);
+    boolean existsByNickname(String nickname);
+}
