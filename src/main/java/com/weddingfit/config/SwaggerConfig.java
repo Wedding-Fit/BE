bc53ec20591755e984198ab173073c107adc64e0
Hyewon Lee <hooho931@gmail.com>
2025-09-08 22:52:37 +0900

    [feat] swagger Authorization 추가 #3

--- src/main/java/com/weddingfit/config/SwaggerConfig.java
+++ src/main/java/com/weddingfit/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.weddingfit.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +13,21 @@ public class SwaggerConfig {
     
     @Bean
     public OpenAPI openAPI() {
+        SecurityRequirement securityRequirement = new SecurityRequirement();
+        securityRequirement.addList("bearerAuth");
+        
         return new OpenAPI()
                 .info(new Info()
                         .title("WeddingFit API")
                         .description("웨딩핏 서비스 API 문서")
-                        .version("v1.0.0"));
+                        .version("v1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", 
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT 인증 토큰을 입력하세요")))
+                .addSecurityItem(securityRequirement);
     }
 }
