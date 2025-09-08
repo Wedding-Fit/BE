79f017a4209e2f29aa7884f7c0444d8bd649ae3b
Hyewon Lee <hooho931@gmail.com>
2025-09-08 18:06:42 +0900

    [feat] 로그아웃 DTO 생성 #3

---
+++ src/main/java/com/weddingfit/dto/request/auth/SignoutRequest.java
@@ -0,0 +1,16 @@
+package com.weddingfit.dto.request.auth;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignoutRequest {
+    @Schema(description = "리프레시 토큰", example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIi...")
+    @NotBlank
+    private String refreshToken;
+}
